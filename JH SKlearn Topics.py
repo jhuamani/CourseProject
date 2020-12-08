8e4655f62a84cbf90ea1c584cd0c8608aea4a899
jhuamani <huamani2@illinois.edu>
2020-12-07 19:17:53 -0800

    Update to SKlearn py

--- JH SKlearn Topics.py
+++ JH SKlearn Topics.py
@@ -4,14 +4,17 @@ import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 from nltk.stem import WordNetLemmatizer
+import pyLDAvis.sklearn
+
 
 def lemma_text(text):
     lemmatizer = WordNetLemmatizer()
     return [lemmatizer.lemmatize(word) for word in text]
 
+
 main_df = pd.read_csv('2000.csv', usecols=['Body', 'Publication Day Of Month', 'Publication Month', 'Publication Year'])
 
-#Remove NaN values, lowercase contents of Body column, filters for bush and gore and resets the index
+# Remove NaN values, lowercase contents of Body column, filters for bush and gore and resets the index
 print(main_df.shape)
 main_df.dropna(subset=['Body'], inplace=True)
 print(main_df.shape)
@@ -20,21 +23,22 @@ main_df['Body'] = main_df['Body'].str.lower()
 main_df = main_df[main_df['Body'].str.contains('gore|bush')]
 main_df = main_df.reset_index(drop=True)
 
-#Create a single date column from day, month and year columns
-main_df['Date'] = pd.to_datetime(main_df['Publication Year']*10000+main_df['Publication Month']*100+main_df['Publication Day Of Month'], format='%Y%m%d')
-main_df.drop(['Publication Year','Publication Month', 'Publication Day Of Month'], axis=1, inplace=True)
+# Create a single date column from day, month and year columns
+main_df['Date'] = pd.to_datetime(
+    main_df['Publication Year'] * 10000 + main_df['Publication Month'] * 100 + main_df['Publication Day Of Month'],
+    format='%Y%m%d')
+main_df.drop(['Publication Year', 'Publication Month', 'Publication Day Of Month'], axis=1, inplace=True)
 print(main_df.shape)
-print(main_df.head(10))
 
-#Temporary check on first N documents
-main_df = main_df[:500]
+# Temporary check on first N documents
+# main_df = main_df[:2000]
 
-#Remove unnecessary symbols, numbers, words less than 3 characters and apply lemmatizer
-main_df['Body'].replace(to_replace='[,\.!?]', value='', inplace=True, regex=True)
-main_df['Body'].replace(to_replace='\d+', value='', inplace=True, regex=True)
-main_df['Body'].replace(to_replace=r'\b(\w{1,2})\b', value='', inplace=True, regex=True)
+# Remove unnecessary symbols, numbers, words less than 3 characters and apply lemmatizer
+main_df['Body'].replace(['[,\.!?]', '\d+', r'\b(\w{1,2})\b'], '', inplace=True, regex=True)
 main_df['Body'].apply(lemma_text)
-
+main_df['Body'] = main_df['Body'].str.replace('said', '', regex=False)
+print(main_df['Body'].head(10))
+"""
 #Generate Wordcloud
 string_check = main_df['Body'].str.cat(sep=' ')
 
@@ -44,27 +48,40 @@ wordcloud.generate(string_check)
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.show()
-
-#Generate doc-term matrix
-cv = CountVectorizer(stop_words='english')
+"""
+# Generate doc-term matrix
+cv = CountVectorizer(stop_words='english', max_df=3500)
 ft_cv = cv.fit_transform(main_df['Body'])
 vocabulary = cv.get_feature_names()
 
 doc_term_matrix = pd.DataFrame(ft_cv.toarray(), columns=vocabulary)
+print(doc_term_matrix.head(20))
 
-#Fit LDA model to doc-term matrix
-lda = LatentDirichletAllocation(n_components=6)
+# Fit LDA model to doc-term matrix
+k = 15
+lda = LatentDirichletAllocation(n_components=k)
+
 lda.fit(ft_cv)
 
-print('log likelihood score: ' + str(lda.score(ft_cv)))
+print('log likelihood score, 15 topics: ' + str(lda.score(ft_cv)))
 
 for i, topic in enumerate(lda.components_):
-    print('topic '+ str(i))
-    print(' '.join([vocabulary[j] for j in topic.argsort()[:-16:-1]]))
+    print('topic ' + str(i))
+    topic_words = [vocabulary[j] for j in topic.argsort()]
+    for word in topic_words[:-4:-1]:
+        print(word)
 
-#Generate doc-topic matrix
+p = pyLDAvis.sklearn.prepare(lda, ft_cv, cv)
+pyLDAvis.save_html(p, 'lda.html')
+
+# Generate doc-topic matrix
 lda_out = lda.transform(ft_cv)
 doc_topic_matrix = pd.DataFrame(lda_out)
 doc_topic_matrix['Date'] = main_df['Date']
 print(doc_topic_matrix.head(100))
 print(doc_topic_matrix.shape)
+
+aggregator = {i: 'sum' for i in range(k)}
+coverage_curve = doc_topic_matrix.groupby(['Date']).agg(aggregator).head(100)
+print(coverage_curve)
+print(coverage_curve.shape)
